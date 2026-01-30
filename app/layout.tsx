47fb5954d1e8b7dc2defc463104e78a67a4562af
Panagiotis Mantas <mantas.pan1@gmail.com>
2026-01-30 11:25:16 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -138,6 +139,18 @@ export default function RootLayout({
   return (
     <html lang="el">
       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-P7QKGFMQF5"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-P7QKGFMQF5');
+          `}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
